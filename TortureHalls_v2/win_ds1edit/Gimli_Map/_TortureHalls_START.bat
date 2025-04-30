0510609cbf22bea766d5a41c64372a9f872da3b7
gimli#4667 on Discord, e-soni@d2jsp <119764208+D2R-Gimli@users.noreply.github.com>
2025-04-30 17:24:54 +0200

    Delete TortureHalls_v2 directory

--- TortureHalls_v2/win_ds1edit/Gimli_Map/_TortureHalls_START.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-D:
-cd "D:/_PD2_Mod/win_ds1edit"
-win_ds1edit.exe Gimli_Map/TortureHalls.ds1 -force_dt1 Sources/Extract/data/global/tiles/expansion/Wildtemple/interior.dt1 Sources/Extract/data/global/tiles/PD2assets/torturehalls/torturehalls.dt1 Sources/Extract/data/global/tiles/expansion/Town/trees.dt1 Sources/Extract/data/global/tiles/PD2assets/torturehalls/a5_torture.dt1 > "Gimli_Map/_debug.txt"
-If ERRORLEVEL 0 goto DONE
-pause
-:DONE
